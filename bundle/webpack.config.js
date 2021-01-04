6b60763c0bd5e57d90bace02f40d4348ff2e24ea
Alsmile <alsmile123@qq.com>
2021-01-04 16:10:04 +0800

    bundle

--- bundle/webpack.config.js
+++ bundle/webpack.config.js
@@ -1,26 +1,35 @@
-const path = require("path");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: "./src",
+  mode: 'production',
+  entry: './src',
   output: {
-    path: path.resolve(__dirname, "."),
-    filename: "../dist/bundle/index.js",
-    library: "MA",
-    libraryTarget: "umd",
+    path: path.resolve(__dirname, '.'),
+    filename: '../dist/bundle/index.js',
+    library: 'MA',
+    libraryTarget: 'umd',
   },
+  target: ['web', 'es5'],
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: ["babel-loader", "ts-loader"],
-        exclude: [path.resolve(__dirname, "node_modules")],
+        use: [
+          'ts-loader',
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+        exclude: [path.resolve(__dirname, 'node_modules')],
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
     plugins: [
       new TsconfigPathsPlugin({
         /* options: see below */
