e837d8af995608af39f119147516ea2cebe25f1e
SignDawn <704425540@qq.com>
2022-02-17 16:29:42 +0800

    in window dont have echarts need to load echarts

--- packages/chart-diagram/src/register.ts
+++ packages/chart-diagram/src/register.ts
@@ -3,7 +3,7 @@ import { echarts, echartsObjs } from './echarts';
 
 export function register(_echarts?: any) {
   echartsObjs.echarts = _echarts;
-  if (!echartsObjs.echarts && !(echarts as any)) {
+  if (!echartsObjs.echarts && !(window as any).echarts) {
     loadJS(
       'https://cdn.bootcdn.net/ajax/libs/echarts/4.8.0/echarts.min.js',
       undefined,
